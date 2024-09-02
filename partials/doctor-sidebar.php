119dcf7283db8e7ba8530a1730bebfc89760df04
rrgarzota <rrgarzota@gmail.com>
2024-09-01 19:37:40 -0500

    web files cleanup

--- partials/doctor-sidebar.php
+++ partials/doctor-sidebar.php
@@ -14,7 +14,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="index.php" class="nav-link" id="alerts">
+                    <a href="index.php?cbResetParam=1" class="nav-link" id="alerts">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>Alerts</p>
                     </a>
