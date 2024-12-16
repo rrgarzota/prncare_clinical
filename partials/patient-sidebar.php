17c43bc688f7b6829a777e354b25f3502e688d0e
rrgarzota <rrgarzota@gmail.com>
2024-12-16 11:15:01 -0600

    Patients > Health Snapshots

---
+++ partials/patient-sidebar.php
@@ -0,0 +1,33 @@
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-blue-theme sidebar-no-expand elevation-4">
+    <!-- Brand Logo -->
+    <a href="../doctor/index.php" class="brand-link navbar-blue-theme ?>">
+        <img src="../assets/img/template/prn-log - Copy.jpeg" alt="Logo" class="brand-image img-circle elevation-2">
+        <span class="brand-text font-weight-bold">Patient</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar pt-3">
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column cb-sidebar nav-flat nav-child-indent" data-widget="treeview" role="menu" data-accordion="false">
+                <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="index.php?cbResetParam=1" class="nav-link" id="alerts">
+                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <p>Health Snapshots</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="manage-messages.php" class="nav-link" id="manage-messages">
+                        <i class="nav-icon fas fa-comment-dots"></i>
+                        <p>Messages</p>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ patient/index.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <?php include '../partials/header.php'; ?>
+    <title>Patient: Dashboard</title>
+
+</head>
+
+<body class="hold-transition layout-sidebar ">
+
+    <!-- A div that wraps the whole site -->
+    <div class="wrapper">
+
+        <!-- Contains the logo and navbar -->
+        <?php include '../partials/topbar.php'; ?>
+        <!-- Contains the user panel and sidebar menu -->
+        <?php include '../partials/patient-sidebar.php'; ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <section class="content-header">
+                <div class="container-fluid">
+                    <div class="row mt-2 mb-1">
+                        <div class="col-lg-6 col-12">
+                            <h1>Health Snapshots</h1>
+                        </div>
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Default box -->
+                            <div class="card">
+                                <!-- search - submission form -->
+                                <div class="card-body submit-d-none blue-theme pt-0 pb-2">
+                                    <script type="text/javascript" src="https://c9ebv091.caspio.com/dp/40c0e00062a72b61421a4e1fa992/emb"></script>
+                                </div>
+                            </div>
+
+                            <div class="search-result-container">
+                                <!-- widgets -->
+                                <div class="card bg-transparent shadow-none border-0 mb-0 widgets-container">
+                                    <div class="card-body bg-transparent px-0">
+                                        <script type="text/javascript" src="https://c9ebv091.caspio.com/dp/40c0e0006024367d4f5442ec97ef/emb"></script>
+                                    </div>
+                                </div> 
+                                <!-- end of widgets -->
+
+                                <!-- calendar container -->
+                                <div class="card mb-5">
+                                    <div class="card-body calendar-report blue-theme one-item-only d-none" id="per_patient">
+                                        <!-- filter per patient -->
+                                        <script type="text/javascript" src="https://c9ebv091.caspio.com/dp/40c0e000b79b5717ecb54af88f8b/emb"></script>
+                                    </div>
+                                    <div class="card-body calendar-report blue-theme one-item-only d-none" id="per_device">
+                                        <!-- filter per device -->
+                                        <script type="text/javascript" src="https://c9ebv091.caspio.com/dp/40c0e000972262eeaf4349c6ab27/emb"></script>
+                                    </div>
+                                </div>
+                                <!-- end of calendar container -->
+                            </div>
+
+                            <!-- /.card -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- footer -->
+        <?php include '../partials/footer.php'; ?>
+        <script>
+            $(function(){
+                // Set side navigation active
+                setActiveNav('alerts');
+
+                // Condition to show message to select in search or result (widdgets/calendar)
+                $perPatient = $('#per_patient');
+                $perDevice = $('#per_device');
+
+                var url = new URL(window.location.href);
+                var search_params = url.searchParams;
+                var device_ID =  search_params.get('DID');
+
+
+                // condition for calendar DataPage
+                if (device_ID == null || device_ID == '') {
+                    // Per patient DataPage Calendar
+                    if ($perPatient.hasClass('d-none')){
+                        $perPatient.removeClass('d-none');
+                    }
+                } else {
+                    // Per patient and device DataPage Calendar
+                    if ($perDevice.hasClass('d-none')){
+                        $perDevice.removeClass('d-none');
+                    }
+                }
+
+               
+            });
+        </script>
+
+    </div>
+    <!-- ./wrapper -->
+
+</body>
+
+</html>
